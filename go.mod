d7a9a7a626fdc5cc67639f1bb4f5fe5cfe90d068
jppade <jppade@googlemail.com>
2024-05-08 09:34:47 +0200

    first working mvp.

--- go.mod
+++ go.mod
@@ -3,19 +3,29 @@ module github.com/diadata-org/decentral-feeder
 go 1.20
 
 require (
+	github.com/diadata-org/diadata v1.4.339
 	github.com/diadata-org/diaprotocol v1.0.0
+	github.com/ethereum/go-ethereum v1.10.10
+	github.com/gorilla/websocket v1.5.0
 	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 )
 
 require (
 	bazil.org/fuse v0.0.0-20200117225306-7b5117fecadc // indirect
 	github.com/AndreasBriese/bbloom v0.0.0-20190825152654-46b345b51c96 // indirect
 	github.com/Jorropo/jsync v1.0.1 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
 	github.com/alexbrainman/goissue34681 v0.0.0-20191006012335-3fc7a47baff5 // indirect
 	github.com/benbjohnson/clock v1.3.5 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
 	github.com/ceramicnetwork/go-dag-jose v0.1.0 // indirect
 	github.com/cespare/xxhash v1.1.0 // indirect
@@ -27,9 +37,10 @@ require (
 	github.com/cskr/pubsub v1.0.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
+	github.com/deckarep/golang-set v1.7.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/dgraph-io/badger v1.6.2 // indirect
-	github.com/dgraph-io/ristretto v0.0.2 // indirect
+	github.com/dgraph-io/ristretto v0.0.3 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/elastic/gosigar v0.14.2 // indirect
@@ -40,20 +51,22 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.1 // indirect
 	github.com/go-logr/logr v1.3.0 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gogo/protobuf v1.3.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/google/gopacket v1.1.19 // indirect
 	github.com/google/pprof v0.0.0-20231023181126-ff6d637d2a7b // indirect
 	github.com/google/uuid v1.4.0 // indirect
-	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/golang-lru v1.0.2 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/holiman/uint256 v1.2.4 // indirect
 	github.com/huin/goupnp v1.3.0 // indirect
 	github.com/ipfs-shipyard/nopfs v0.0.12-0.20231027223058-cde3b5ba964c // indirect
 	github.com/ipfs-shipyard/nopfs/ipfs v0.13.2-0.20231027223058-cde3b5ba964c // indirect
@@ -165,10 +178,15 @@ require (
 	github.com/quic-go/quic-go v0.39.4 // indirect
 	github.com/quic-go/webtransport-go v0.6.0 // indirect
 	github.com/raulk/go-watchdog v1.3.0 // indirect
+	github.com/rjeczalik/notify v0.9.2 // indirect
 	github.com/samber/lo v1.39.0 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	github.com/tidwall/gjson v1.17.1
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/ucarion/urlpath v0.0.0-20200424170820-7ccc79b76bbb // indirect
 	github.com/whyrusleeping/base32 v0.0.0-20170828182744-c30ac30633cc // indirect
 	github.com/whyrusleeping/cbor v0.0.0-20171005072247-63513f603b11 // indirect
@@ -195,23 +213,29 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 	go4.org v0.0.0-20230225012048-214862532bf5 // indirect
-	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
-	golang.org/x/mod v0.14.0 // indirect
-	golang.org/x/net v0.19.0 // indirect
-	golang.org/x/sync v0.5.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/mod v0.17.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.16.0 // indirect
+	golang.org/x/tools v0.20.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gonum.org/v1/gonum v0.14.0 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
-	google.golang.org/protobuf v1.31.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.2.1 // indirect
 )
 
-replace github.com/diadata-org/diaprotocol => /home/philipp/go/src/github.com/diadata-org/diaprotocol
+replace github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
+
+
+
 replace github.com/diadata-org/decentral-feeder => /home/philipp/go/src/github.com/diadata-org/decentral-feeder
+
+replace github.com/diadata-org/diadata => /home/philipp/go/src/github.com/diadata-org/diadata
--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"context"
+	"io/ioutil"
+	"math/big"
+	"net/http"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/diadata-org/decentral-feeder/pkg/onchain"
 	processing "github.com/diadata-org/decentral-feeder/pkg/processing"
 	scrapers "github.com/diadata-org/decentral-feeder/pkg/scraper"
+	utils "github.com/diadata-org/decentral-feeder/pkg/utils"
+	diaOracleV2MultiupdateService "github.com/diadata-org/diadata/pkg/dia/scraper/blockchain-scrapers/blockchains/ethereum/diaOracleV2MultiupdateService"
 	models "github.com/diadata-org/diaprotocol/pkg/models"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
+	"github.com/tidwall/gjson"
 )
 
 func main() {
@@ -16,12 +28,49 @@ func main() {
 
 	wg := sync.WaitGroup{}
 	tradesblockChannel := make(chan []models.Trade)
-	filtersChannel := make(chan float64)
+	filterChannel := make(chan models.FilterPoint)
 	triggerChannel := make(chan time.Time)
+
+	// ----------------------------
+	// Feeder mechanics
+	// ----------------------------
+	key := utils.Getenv("PRIVATE_KEY", "")
+	key_password := utils.Getenv("PRIVATE_KEY_PASSWORD", "")
+	deployedContract := utils.Getenv("DEPLOYED_CONTRACT", "")
+	blockchainNode := utils.Getenv("BLOCKCHAIN_NODE", "https://rpc2.sepolia.org")
+	backupNode := utils.Getenv("BACKUP_NODE", "https://rpc.sepolia.ethpandaops.io")
+
+	conn, err := ethclient.Dial(blockchainNode)
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	connBackup, err := ethclient.Dial(backupNode)
+	if err != nil {
+		log.Fatalf("Failed to connect to the backup Ethereum client: %v", err)
+	}
+	chainId, err := strconv.ParseInt(utils.Getenv("CHAIN_ID", "11155111"), 10, 64)
+	if err != nil {
+		log.Fatalf("Failed to parse chainId: %v", err)
+	}
+	frequencySeconds, err := strconv.Atoi(utils.Getenv("FREQUENCY_SECONDS", "20"))
+	if err != nil {
+		log.Fatalf("Failed to parse frequencySeconds: %v", err)
+	}
+
+	auth, err := bind.NewTransactorWithChainID(strings.NewReader(key), key_password, big.NewInt(chainId))
+	if err != nil {
+		log.Fatalf("Failed to create authorized transactor: %v", err)
+	}
+
+	var contract, contractBackup *diaOracleV2MultiupdateService.DiaOracleV2MultiupdateService
+	err = onchain.DeployOrBindContract(deployedContract, conn, connBackup, auth, &contract, &contractBackup)
+	if err != nil {
+		log.Fatalf("Failed to Deploy or Bind primary and backup contract: %v", err)
+	}
 
 	// Use a ticker for triggering the processing.
 	// This is for testing purposes for now.
-	triggerTick := time.NewTicker(time.Duration(5 * time.Second))
+	triggerTick := time.NewTicker(time.Duration(frequencySeconds) * time.Second)
 	go func() {
 		for tick := range triggerTick.C {
 			log.Warn("tick at: ", tick)
@@ -30,31 +79,65 @@ func main() {
 	}()
 
 	// Run Processor and subsequent routines.
-	Processor(exchanges, pairs, tradesblockChannel, filtersChannel, triggerChannel, &wg)
+	go Processor(exchanges, pairs, tradesblockChannel, filterChannel, triggerChannel, &wg)
+
+	// Outlook/Alternative: The triggerChannel can also be filled by the oracle updater by any other mechanism.
+	// oracleUpdateExecutor(auth, contract, conn, chainId, filterChannel)
+	oracleUpdateExecutor(auth, contract, conn, chainId, filterChannel)
+
+}
+
+func oracleUpdateExecutor(
+	// publishedPrices map[string]float64,
+	// newPrices map[string]float64,
+	// deviationPermille int,
+	auth *bind.TransactOpts,
+	contract *diaOracleV2MultiupdateService.DiaOracleV2MultiupdateService,
+	conn *ethclient.Client,
+	chainId int64,
+	// compatibilityMode bool,
+	filterChannel <-chan models.FilterPoint,
+) {
+
+	timestamp := time.Now().Unix()
+	for filterPoint := range filterChannel {
+		log.Infof("%v -- filterPoint received: %v -- %v", time.Unix(timestamp, 0), filterPoint.Value, filterPoint.Time)
+		err := updateOracleMultiValues(conn, contract, auth, chainId, []string{"BTC/USD"}, []int64{int64(filterPoint.Value * 100000000)}, timestamp)
+		if err != nil {
+			log.Printf("Failed to update Oracle: %v", err)
+			return
+		}
+	}
 
 }
 
 func Processor(exchanges []models.Exchange,
 	pairs []string,
 	tradesblockChannel chan []models.Trade,
-	filterChannel chan float64,
+	filterChannel chan models.FilterPoint,
 	triggerChannel chan time.Time,
 	wg *sync.WaitGroup,
 ) {
 
+	log.Info("Start Processor......")
 	// Collector starts collecting trades in the background.
 	go Collector(exchanges, pairs, tradesblockChannel, triggerChannel, wg)
 
 	// As soon as the trigger channel receives input a processing step is initiated.
-	for range triggerChannel {
-		trades := <-tradesblockChannel
+	for trades := range tradesblockChannel {
+
 		log.Infof("received %v trades for further processing.", len(trades))
 		asset := models.Asset{Blockchain: "Bitcoin", Address: "0x0000000000000000000000000000000000000000"}
 		latestPrice, timestamp, err := processing.LastPrice(trades, asset, true)
 		if err != nil {
 			log.Error("GetLastPrice: ", err)
 		}
-		log.Infof("LatestPrice -- timestamp: %v -- %v", latestPrice, timestamp)
+		filterPoint := models.FilterPoint{
+			Asset: asset,
+			Value: latestPrice,
+			Time:  timestamp,
+		}
+		filterChannel <- filterPoint
 	}
 
 }
@@ -83,8 +166,8 @@ func Collector(
 				collectedTrades = append(collectedTrades, trade)
 
 			case timestamp := <-triggerChannel:
-				tradesblockChannel <- collectedTrades
 				log.Info("triggered at : ", timestamp)
+				tradesblockChannel <- collectedTrades
 				collectedTrades = []models.Trade{}
 
 			}
@@ -92,4 +175,80 @@ func Collector(
 	}()
 
 	defer wg.Wait()
+}
+
+func updateOracleMultiValues(
+	client *ethclient.Client,
+	contract *diaOracleV2MultiupdateService.DiaOracleV2MultiupdateService,
+	auth *bind.TransactOpts,
+	chainId int64,
+	keys []string,
+	values []int64,
+	timestamp int64) error {
+
+	var cValues []*big.Int
+	var gasPrice *big.Int
+	var err error
+
+	// Get proper gas price depending on chainId
+	switch chainId {
+	/*case 288: //Boba
+	gasPrice = big.NewInt(1000000000)*/
+	case 592: //Astar
+		response, err := http.Get("https://gas.astar.network/api/gasnow?network=astar")
+		if err != nil {
+			return err
+		}
+
+		defer response.Body.Close()
+		if 200 != response.StatusCode {
+			return err
+		}
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return err
+		}
+
+		gasSuggestion := gjson.Get(string(contents), "data.fast")
+		gasPrice = big.NewInt(gasSuggestion.Int())
+	default:
+		// Get gas price suggestion
+		gasPrice, err = client.SuggestGasPrice(context.Background())
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+
+		// Get 110% of the gas price
+		fGas := new(big.Float).SetInt(gasPrice)
+		fGas.Mul(fGas, big.NewFloat(1.1))
+		gasPrice, _ = fGas.Int(nil)
+	}
+
+	for _, value := range values {
+		// Create compressed argument with values/timestamps
+		cValue := big.NewInt(value)
+		cValue = cValue.Lsh(cValue, 128)
+		cValue = cValue.Add(cValue, big.NewInt(timestamp))
+		cValues = append(cValues, cValue)
+	}
+
+	// Write values to smart contract
+	tx, err := contract.SetMultipleValues(&bind.TransactOpts{
+		From:     auth.From,
+		Signer:   auth.Signer,
+		GasPrice: gasPrice,
+	}, keys, cValues)
+	// check if tx is sendable then fgo backup
+	if err != nil {
+		// backup in here
+		return err
+	}
+
+	log.Printf("Gas price: %d\n", tx.GasPrice())
+	log.Printf("Data: %x\n", tx.Data())
+	log.Printf("Nonce: %d\n", tx.Nonce())
+	log.Printf("Tx To: %s\n", tx.To().String())
+	log.Printf("Tx Hash: 0x%x\n", tx.Hash())
+	return nil
 }
