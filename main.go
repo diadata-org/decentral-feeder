ec1987d3c309194f43c392f8df606ec4a5ba81a1
jppade <jppade@googlemail.com>
2024-07-17 16:55:36 +0200

    isolate onchain functions.

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"io/ioutil"
 	"math/big"
-	"net/http"
 	"strconv"
 	"strings"
 	"sync"
@@ -20,7 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/ethclient"
 	log "github.com/sirupsen/logrus"
-	"github.com/tidwall/gjson"
 )
 
 const (
@@ -155,118 +151,5 @@ func main() {
 	go processor.Processor(exchangePairs, pools, tradesblockChannel, filtersChannel, triggerChannel, &wg)
 
 	// Outlook/Alternative: The triggerChannel can also be filled by the oracle updater by any other mechanism.
-	oracleUpdateExecutor(auth, contract, conn, chainId, filtersChannel)
-}
-
-func oracleUpdateExecutor(
-	// publishedPrices map[string]float64,
-	// newPrices map[string]float64,
-	// deviationPermille int,
-	auth *bind.TransactOpts,
-	contract *diaOracleV2MultiupdateService.DiaOracleV2MultiupdateService,
-	conn *ethclient.Client,
-	chainId int64,
-	// compatibilityMode bool,
-	filtersChannel <-chan []models.FilterPointExtended,
-) {
-
-	for filterPoints := range filtersChannel {
-		timestamp := time.Now().Unix()
-		var keys []string
-		var values []int64
-		for _, fp := range filterPoints {
-			log.Infof(
-				"filterPoint received at %v: %v -- %v -- %v -- %v",
-				time.Unix(timestamp, 0),
-				fp.Source,
-				fp.Pair.QuoteToken.Symbol+"-"+fp.Pair.BaseToken.Symbol,
-				fp.Value,
-				fp.Time,
-			)
-			keys = append(keys, fp.Pair.QuoteToken.Symbol+"/USD")
-			values = append(values, int64(fp.Value*100000000))
-		}
-		err := updateOracleMultiValues(conn, contract, auth, chainId, keys, values, timestamp)
-		if err != nil {
-			log.Printf("Failed to update Oracle: %v", err)
-			return
-		}
-	}
-
-}
-
-func updateOracleMultiValues(
-	client *ethclient.Client,
-	contract *diaOracleV2MultiupdateService.DiaOracleV2MultiupdateService,
-	auth *bind.TransactOpts,
-	chainId int64,
-	keys []string,
-	values []int64,
-	timestamp int64) error {
-
-	var cValues []*big.Int
-	var gasPrice *big.Int
-	var err error
-
-	// Get proper gas price depending on chainId
-	switch chainId {
-	/*case 288: //Bobapkg/scraper/Uniswapv2.go
-	gasPrice = big.NewInt(1000000000)*/
-	case 592: //Astar
-		response, err := http.Get("https://gas.astar.network/api/gasnow?network=astar")
-		if err != nil {
-			return err
-		}
-
-		defer response.Body.Close()
-		if 200 != response.StatusCode {
-			return err
-		}
-		contents, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return err
-		}
-
-		gasSuggestion := gjson.Get(string(contents), "data.fast")
-		gasPrice = big.NewInt(gasSuggestion.Int())
-	default:
-		// Get gas price suggestion
-		gasPrice, err = client.SuggestGasPrice(context.Background())
-		if err != nil {
-			log.Print(err)
-			return err
-		}
-
-		// Get 110% of the gas price
-		fGas := new(big.Float).SetInt(gasPrice)
-		fGas.Mul(fGas, big.NewFloat(1.1))
-		gasPrice, _ = fGas.Int(nil)
-	}
-
-	for _, value := range values {
-		// Create compressed argument with values/timestamps
-		cValue := big.NewInt(value)
-		cValue = cValue.Lsh(cValue, 128)
-		cValue = cValue.Add(cValue, big.NewInt(timestamp))
-		cValues = append(cValues, cValue)
-	}
-
-	// Write values to smart contract
-	tx, err := contract.SetMultipleValues(&bind.TransactOpts{
-		From:     auth.From,
-		Signer:   auth.Signer,
-		GasPrice: gasPrice,
-	}, keys, cValues)
-	// check if tx is sendable then fgo backup
-	if err != nil {
-		// backup in here
-		return err
-	}
-
-	log.Printf("Gas price: %d\n", tx.GasPrice())
-	log.Printf("Data: %x\n", tx.Data())
-	log.Printf("Nonce: %d\n", tx.Nonce())
-	log.Printf("Tx To: %s\n", tx.To().String())
-	log.Printf("Tx Hash: 0x%x\n", tx.Hash())
-	return nil
+	onchain.OracleUpdateExecutor(auth, contract, conn, chainId, filtersChannel)
 }
