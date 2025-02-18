dc874abbba19fc8bf84b8ec69d21b5a41cf7f824
nnn-gif <nicky.gurbani@gmail.com>
2025-02-18 12:15:21 +0530

    add tests for metadata

--- contracts/DIAOracleV2Meta.sol
+++ contracts/DIAOracleV2Meta.sol
@@ -8,23 +8,46 @@ interface IDIAOracleV2 {
     function updateOracleUpdaterAddress(address newOracleUpdaterAddress) external;
 }
 
+/**
+ * @title DIAOracleV2Meta
+ */
 contract DIAOracleV2Meta {
+    /// @notice Mapping of registered oracle addresses.
     mapping (uint256 => address) oracles;
-    uint256 numOracles;
-    uint256 threshold;
-    uint256 timeoutSeconds;
 
+    /// @notice Number of registered oracles.
+    uint256 private numOracles;
+    
+    /// @notice Minimum number of valid values required to return a result.
+    uint256 private threshold;
+
+    /// @notice The timeout period in seconds for oracle values.
+    uint256 private timeoutSeconds;
+
+    /// @notice Address of the administrator who can manage oracles.
     address admin;
 
     constructor() {
         admin = msg.sender;
     }
 
+    /**
+     * @notice Adds a new oracle to the registry.
+     * @dev Only the administrator can call this function.
+     * @param newOracleAddress The address of the oracle contract to add.
+     */
     function addOracle(address newOracleAddress) public {
         require(msg.sender == admin);
         oracles[numOracles] = newOracleAddress;
         numOracles += 1;
     }
+
+
+    /**
+     * @notice Removes an oracle from the registry.
+     * @dev Only the administrator can call this function.
+     * @param oracleToRemove The address of the oracle contract to remove.
+     */
 
     function removeOracle(address oracleToRemove) public {
         require(msg.sender == admin);
@@ -38,15 +61,35 @@ contract DIAOracleV2Meta {
         }
     }
 
+
+     /**
+     * @notice Sets the required threshold of valid oracle values.
+     * @dev Only the administrator can call this function.
+     * @param newThreshold The new threshold value.
+     */
+
     function setThreshold(uint256 newThreshold) public {
         require(msg.sender == admin);
         threshold = newThreshold;
     }
 
+ /**
+     * @notice Sets the timeout period for oracle values.
+     * @dev Only the administrator can call this function.
+     * @param newTimeoutSeconds The new timeout period in seconds.
+     */
     function setTimeoutSeconds(uint256 newTimeoutSeconds) public {
         require(msg.sender == admin);
         timeoutSeconds = newTimeoutSeconds;
     }
+
+    /**
+     * @notice Retrieves the median price value for a given asset key from registered oracles.
+     * @dev Only returns values that are not older than the timeout period.
+     * @param key The asset identifier (e.g., "BTC/USD").
+     * @return value The median price from available oracles.
+     * @return timestamp The current block timestamp.
+     */
 
     function getValue(string memory key) external view returns (uint128, uint128) {
         require(timeoutSeconds > 0);
@@ -80,5 +123,17 @@ contract DIAOracleV2Meta {
         uint256 medianIndex = validValues / 2;
 
         return (values[medianIndex], uint128(block.timestamp));
+    }
+
+     function getNumOracles() external view returns (uint256) {
+        return numOracles;
+    }
+
+    function getThreshold() external view returns (uint256) {
+        return threshold;
+    }
+
+    function getTimeoutSeconds() external view returns (uint256) {
+        return timeoutSeconds;
     }
 }
---
+++ test-foundry/DIAOracleV2metadata.t.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import "forge-std/Test.sol";
+import "../contracts/DIAOracleV2Meta.sol";
+
+// Mock Oracle contract implementing IDIAOracleV2
+contract MockDIAOracle is IDIAOracleV2 {
+    uint128 private storedValue;
+    uint128 private storedTimestamp;
+
+    function setValue(
+        string memory,
+        uint128 value,
+        uint128 timestamp
+    ) external override {
+        storedValue = value;
+        storedTimestamp = timestamp;
+    }
+
+    function getValue(
+        string memory
+    ) external view override returns (uint128, uint128) {
+        return (storedValue, storedTimestamp);
+    }
+
+    function updateOracleUpdaterAddress(address) external override {}
+}
+
+// Forge test contract
+contract DIAOracleV2MetaTest is Test {
+    DIAOracleV2Meta public oracleMeta;
+    MockDIAOracle public oracle1;
+    MockDIAOracle public oracle2;
+    MockDIAOracle public oracle3;
+
+    address public admin = address(0x123);
+
+    function setUp() public {
+        vm.startPrank(admin);
+        oracleMeta = new DIAOracleV2Meta();
+        oracle1 = new MockDIAOracle();
+        oracle2 = new MockDIAOracle();
+        oracle3 = new MockDIAOracle();
+        vm.stopPrank();
+    }
+
+    function testAddOracle() public {
+        vm.startPrank(admin);
+
+        oracleMeta.addOracle(address(oracle1));
+        oracleMeta.addOracle(address(oracle2));
+
+        // Assert that numOracles increased
+        assertEq(oracleMeta.getNumOracles(), 2);
+
+        vm.stopPrank();
+    }
+
+    function testRemoveOracle() public {
+        vm.startPrank(admin);
+
+        oracleMeta.addOracle(address(oracle1));
+        oracleMeta.addOracle(address(oracle2));
+        oracleMeta.addOracle(address(oracle3));
+
+        // Remove oracle2 and check numOracles
+        oracleMeta.removeOracle(address(oracle2));
+        assertEq(oracleMeta.getNumOracles(), 2);
+
+        vm.stopPrank();
+    }
+
+    function testSetThreshold() public {
+        vm.startPrank(admin);
+
+        oracleMeta.setThreshold(2);
+        assertEq(oracleMeta.getThreshold(), 2);
+
+        vm.stopPrank();
+    }
+
+    function testSetTimeout() public {
+        vm.startPrank(admin);
+
+        oracleMeta.setTimeoutSeconds(100);
+        assertEq(oracleMeta.getTimeoutSeconds(), 100);
+
+        vm.stopPrank();
+    }
+
+    function testGetValueMedian() public {
+        vm.startPrank(admin);
+
+        oracleMeta.addOracle(address(oracle1));
+        oracleMeta.addOracle(address(oracle2));
+        oracleMeta.addOracle(address(oracle3));
+
+        oracleMeta.setThreshold(2);
+        oracleMeta.setTimeoutSeconds(1000);
+
+        vm.stopPrank();
+
+        // Set values in oracles
+        oracle1.setValue("BTC", 100, uint128(block.timestamp));
+        oracle2.setValue("BTC", 200, uint128(block.timestamp));
+        oracle3.setValue("BTC", 300, uint128(block.timestamp));
+
+        // Fetch median value
+        (uint128 value, uint128 timestamp) = oracleMeta.getValue("BTC");
+
+        assertEq(value, 200, "Median should be 200");
+        assertEq(
+            timestamp,
+            uint128(block.timestamp),
+            "Timestamp should match current time"
+        );
+    }
+
+
+    function testGetValueFailsWithoutEnoughOracles() public {
+        vm.startPrank(admin);
+
+        oracleMeta.addOracle(address(oracle1));
+        oracleMeta.setThreshold(2);
+        oracleMeta.setTimeoutSeconds(100);
+
+        vm.stopPrank();
+
+        // Set value
+        oracle1.setValue("BTC", 100, uint128(block.timestamp));
+
+        // Expect revert due to insufficient valid oracles
+        vm.expectRevert();
+        oracleMeta.getValue("BTC");
+    }
+}
