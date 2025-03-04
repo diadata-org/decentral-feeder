42017d389e836162e980017104fb3fc45babd702
jppade <jppade@googlemail.com>
2025-03-04 15:49:27 +0100

    add logging to simulation feeder.

--- pkg/simulations/simulators/Collector.go
+++ pkg/simulations/simulators/Collector.go
@@ -23,6 +23,10 @@ func Collector(
 
 	for exchange := range exchangepairMap {
 		wg.Add(1)
+		log.Info("exchangepairMap[exchange] in collector... ")
+		for _, ep := range exchangepairMap[exchange] {
+			log.Infof("exchangepair in collector: %s:%s-%s:%s ... ", ep.UnderlyingPair.QuoteToken.Blockchain, ep.UnderlyingPair.QuoteToken.Address, ep.UnderlyingPair.BaseToken.Blockchain, ep.UnderlyingPair.BaseToken.Address)
+		}
 		go RunSimulator(context.Background(), exchange, exchangepairMap[exchange], tradesChannelIn, wg)
 	}
 
