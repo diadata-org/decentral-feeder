d0bc085781aec1efec6b877d64bd054d2d597fdb
shpookas <vitas.spokas@protonmail.com>
2025-03-11 12:50:29 +0100

    monitoring for simulation pods

--- cmd/simulators/main.go
+++ cmd/simulators/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"math/big"
+	"os"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
+
+	"runtime"
 
 	models "github.com/diadata-org/decentral-feeder/pkg/models"
 	"github.com/diadata-org/decentral-feeder/pkg/onchain"
@@ -16,6 +19,8 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
+	"github.com/shirou/gopsutil/cpu"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -41,85 +46,86 @@ type metrics struct {
 	cpuUsage       prometheus.Gauge
 	memoryUsage    prometheus.Gauge
 	contract       *prometheus.GaugeVec
-	exchangePairs  *prometheus.GaugeVec
-	pools          *prometheus.GaugeVec
+	dexpairs       *prometheus.GaugeVec
 	pushGatewayURL string
 	jobName        string
 	authUser       string
 	authPassword   string
 }
 
-// func NewMetrics(reg prometheus.Registerer, pushGatewayURL, jobName, authUser, authPassword string) *metrics {
-// 	m := &metrics{
-// 		uptime: prometheus.NewGauge(prometheus.GaugeOpts{
-// 			Namespace: "feeder",
-// 			Name:      "uptime_hours",
-// 			Help:      "Feeder Uptime in hours.",
-// 		}),
-// 		cpuUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-// 			Namespace: "feeder",
-// 			Name:      "cpu_usage_percent",
-// 			Help:      "Feeder CPU usage in percent.",
-// 		}),
-// 		memoryUsage: prometheus.NewGauge(prometheus.GaugeOpts{
-// 			Namespace: "feeder",
-// 			Name:      "memory_usage_megabytes",
-// 			Help:      "Feeder Memory usage in megabytes.",
-// 		}),
-// 		contract: prometheus.NewGaugeVec(
-// 			prometheus.GaugeOpts{
-// 				Namespace: "feeder",
-// 				Name:      "contract_info",
-// 				Help:      "Feeder contract information.",
-// 			},
-// 			[]string{"contract"}, // Label to store the contract address
-// 		),
-// 		exchangePairs: prometheus.NewGaugeVec(
-// 			prometheus.GaugeOpts{
-// 				Namespace: "feeder",
-// 				Name:      "exchange_pairs",
-// 				Help:      "List of exchange pairs to be pushed as labels for each Feeder.",
-// 			},
-// 			[]string{"exchange_pair"}, // Label to store each exchange pair
-// 		),
-// 		pools: prometheus.NewGaugeVec(
-// 			prometheus.GaugeOpts{
-// 				Namespace: "feeder",
-// 				Name:      "pools",
-// 				Help:      "List of pools to be pushed as labels for each Feeder.",
-// 			},
-// 			[]string{"exchange", "pool_address"}, // Labels for the exchange and pool address
-// 		),
-// 		pushGatewayURL: pushGatewayURL,
-// 		jobName:        jobName,
-// 		authUser:       authUser,
-// 		authPassword:   authPassword,
-// 	}
-// 	reg.MustRegister(m.uptime)
-// 	reg.MustRegister(m.cpuUsage)
-// 	reg.MustRegister(m.memoryUsage)
-// 	reg.MustRegister(m.contract)
-// 	reg.MustRegister(m.pools)
-// 	return m
-// }
+func NewMetrics(reg prometheus.Registerer, pushGatewayURL, jobName, authUser, authPassword string) *metrics {
+	m := &metrics{
+		uptime: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "feeder",
+			Name:      "uptime_hours",
+			Help:      "Feeder Uptime in hours.",
+		}),
+		cpuUsage: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "feeder",
+			Name:      "cpu_usage_percent",
+			Help:      "Feeder CPU usage in percent.",
+		}),
+		memoryUsage: prometheus.NewGauge(prometheus.GaugeOpts{
+			Namespace: "feeder",
+			Name:      "memory_usage_megabytes",
+			Help:      "Feeder Memory usage in megabytes.",
+		}),
+		contract: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "feeder",
+				Name:      "contract_info",
+				Help:      "Feeder contract information.",
+			},
+			[]string{"contract"}, // Label to store the contract address
+		),
+		dexpairs: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: "feeder",
+				Name:      "dexpairs",
+				Help:      "List of DEX pairs to be pushed as labels for each Feeder.",
+			},
+			[]string{"dex_pair"}, // Label to store each DEX pair
+		),
+		pushGatewayURL: pushGatewayURL,
+		jobName:        jobName,
+		authUser:       authUser,
+		authPassword:   authPassword,
+	}
+	reg.MustRegister(m.uptime)
+	reg.MustRegister(m.cpuUsage)
+	reg.MustRegister(m.memoryUsage)
+	reg.MustRegister(m.contract)
+	reg.MustRegister(m.dexpairs)
+	return m
+}
 
 func init() {
 
-	// Extract exchangepairs from env var.
+	// Extract dexpairs from env var.
 	for _, p := range strings.Split(pairsEnv, ENV_SEPARATOR) {
 		var pair models.ExchangePair
-		pair.Exchange = strings.Trim(strings.Split(p, EXCHANGE_SEPARATOR)[0], " ")
-		pair.UnderlyingPair.QuoteToken.Address = strings.Split(strings.Split(p, EXCHANGE_SEPARATOR)[2], PAIR_SEPARATOR)[0]
-		pair.UnderlyingPair.BaseToken.Address = strings.Split(strings.Split(p, EXCHANGE_SEPARATOR)[2], PAIR_SEPARATOR)[1]
-		pair.UnderlyingPair.QuoteToken.Blockchain = strings.Split(p, EXCHANGE_SEPARATOR)[1]
-		pair.UnderlyingPair.BaseToken.Blockchain = strings.Split(p, EXCHANGE_SEPARATOR)[1]
+		parts := strings.Split(p, EXCHANGE_SEPARATOR)
+		if len(parts) < 3 {
+			log.Warnf("Invalid DEX pair format: %s", p)
+			continue
+		}
+		pair.Exchange = strings.TrimSpace(parts[0])
+		pair.UnderlyingPair.QuoteToken.Blockchain = parts[1]
+		addresses := strings.Split(parts[2], PAIR_SEPARATOR)
+		if len(addresses) < 2 {
+			log.Warnf("Invalid address format in DEX pair: %s", p)
+			continue
+		}
+		pair.UnderlyingPair.QuoteToken.Address = addresses[0]
+		pair.UnderlyingPair.BaseToken.Address = addresses[1]
+		pair.UnderlyingPair.BaseToken.Blockchain = parts[1]
 		exchangePairs = append(exchangePairs, pair)
 		log.Infof(
-			"exchange -- blockchain -- address0 -- address1: %s -- %s -- %s -- %s",
+			"dex -- blockchain -- address0 -- address1: %s -- %s -- %s -- %s",
 			pair.Exchange,
-			strings.Split(p, EXCHANGE_SEPARATOR)[1],
-			strings.Split(strings.Split(p, EXCHANGE_SEPARATOR)[2], PAIR_SEPARATOR)[0],
-			strings.Split(strings.Split(p, EXCHANGE_SEPARATOR)[2], PAIR_SEPARATOR)[1],
+			parts[1],
+			addresses[0],
+			addresses[1],
 		)
 	}
 	for _, ep := range exchangePairs {
@@ -130,77 +136,63 @@ func init() {
 
 func main() {
 
-	// get hostname of the container so that we can display it in monitoring dashboards
-	// hostname, err := os.Hostname()
-	// if err != nil {
-	// 	log.Fatalf("Failed to get hostname: %v", err)
-	// }
-	// // get pushgatewayURL variable from kubernetes env variables, if not, the default is https://pushgateway-auth.diadata.org
-	// pushgatewayURL := utils.Getenv("PUSHGATEWAY_URL", "https://pushgateway-auth.diadata.org")
-	// authUser := os.Getenv("PUSHGATEWAY_USER")
-	// authPassword := os.Getenv("PUSHGATEWAY_PASSWORD")
+	// Get hostname of the container so that we can display it in monitoring dashboards
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("Failed to get hostname: %v", err)
+	}
+	// Get pushgatewayURL variable from Kubernetes env variables, if not, the default is https://pushgateway-auth.diadata.org
+	pushgatewayURL := utils.Getenv("PUSHGATEWAY_URL", "https://pushgateway-auth.diadata.org")
+	authUser := os.Getenv("PUSHGATEWAY_USER")
+	authPassword := os.Getenv("PUSHGATEWAY_PASSWORD")
 
-	// reg := prometheus.NewRegistry()
-	// m := NewMetrics(reg, pushgatewayURL, "df_"+hostname, authUser, authPassword)
+	reg := prometheus.NewRegistry()
+	m := NewMetrics(reg, pushgatewayURL, "df_"+hostname, authUser, authPassword)
 
 	// Record start time for uptime calculation
-	// startTime := time.Now()
+	startTime := time.Now()
 
 	// Get deployed contract and set the metric
 	deployedContract := utils.Getenv("DEPLOYED_CONTRACT", "")
-	// Set the static contract label for Prometheus monitoring
-	// m.contract.WithLabelValues(deployedContract).Set(1) // The value is arbitrary; the label holds the address
+	m.contract.WithLabelValues(deployedContract).Set(1) // The value is arbitrary; the label holds the address
 
-	// // Iterate through the pools slice and set values for the pools metric. Push only if pools are available.
-	// if len(pools) > 0 {
-	// 	for _, pool := range pools {
-	// 		m.pools.WithLabelValues(pool.Exchange.Name, pool.Address).Set(1)
-	// 	}
-	// } else {
-	// 	log.Info("No pools to push metrics for; POOLS environment variable is empty.")
-	// }
+	// Periodically update and push metrics to pushgateway
+	go func() {
+		for {
+			uptime := time.Since(startTime).Hours()
+			m.uptime.Set(uptime)
 
-	// // Periodically update and push metrics to pushgateway
-	// go func() {
-	// 	for {
-	// 		uptime := time.Since(startTime).Hours()
-	// 		m.uptime.Set(uptime)
+			// Update memory usage
+			var memStats runtime.MemStats
+			runtime.ReadMemStats(&memStats)
+			memoryUsageMB := float64(memStats.Alloc) / 1024 / 1024 // Convert bytes to megabytes
+			m.memoryUsage.Set(memoryUsageMB)
 
-	// 		// Update memory usage
-	// 		var memStats runtime.MemStats
-	// 		runtime.ReadMemStats(&memStats)
-	// 		memoryUsageMB := float64(memStats.Alloc) / 1024 / 1024 // Convert bytes to megabytes
-	// 		m.memoryUsage.Set(memoryUsageMB)
+			// Update CPU usage using gopsutil
+			percent, _ := cpu.Percent(0, false)
+			if len(percent) > 0 {
+				m.cpuUsage.Set(percent[0])
+			}
 
-	// 		// Update CPU usage using gopsutil
-	// 		percent, _ := cpu.Percent(0, false)
-	// 		if len(percent) > 0 {
-	// 			m.cpuUsage.Set(percent[0])
-	// 		}
+			// Push metrics to the Pushgateway
+			pushCollector := push.New(m.pushGatewayURL, m.jobName).
+				Collector(m.uptime).
+				Collector(m.cpuUsage).
+				Collector(m.memoryUsage).
+				Collector(m.contract).
+				Collector(m.dexpairs)
 
-	// 		// Push metrics to the Pushgateway
-	// 		pushCollector := push.New(m.pushGatewayURL, m.jobName).
-	// 			Collector(m.uptime).
-	// 			Collector(m.cpuUsage).
-	// 			Collector(m.memoryUsage).
-	// 			Collector(m.contract).
-	// 			Collector(m.exchangePairs)
+			if err := pushCollector.
+				BasicAuth(m.authUser, m.authPassword).
+				Push(); err != nil {
+				log.Errorf("Could not push metrics to Pushgateway: %v", err)
+			} else {
+				log.Printf("Metrics pushed successfully to Pushgateway")
+			}
 
-	// 		if len(pools) > 0 {
-	// 			pushCollector = pushCollector.Collector(m.pools)
-	// 		}
-
-	// 		if err := pushCollector.
-	// 			BasicAuth(m.authUser, m.authPassword).
-	// 			Push(); err != nil {
-	// 			log.Errorf("Could not push metrics to Pushgateway: %v", err)
-	// 		} else {
-	// 			log.Printf("Metrics pushed successfully to Pushgateway")
-	// 		}
-
-	// 		time.Sleep(30 * time.Second) // update metrics every 30 seconds
-	// 	}
-	// }()
+			time.Sleep(30 * time.Second) // update metrics every 30 seconds
+		}
+	}()
 
 	wg := sync.WaitGroup{}
 	tradesblockChannel := make(chan map[string]models.SimulatedTradesBlock)
