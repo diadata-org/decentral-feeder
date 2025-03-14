d4cc3941a60219c48a1e5aa3d5d1a29faf51f0a3
Samuel Brack <samuel.brack@diadata.org>
2025-03-14 14:42:36 +0100

    add RBAC to DIAOracle contract

--- contracts/DIAOracleV2.sol
+++ contracts/DIAOracleV2.sol
@@ -1,11 +1,10 @@
-// compiled using solidity 0.8.19
-
 pragma solidity 0.8.26;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 interface IDIAOracleV2 {                                                           
     function setValue(string memory key, uint128 value, uint128 timestamp) external;
     function getValue(string memory key) external view returns (uint128, uint128);
-    function updateOracleUpdaterAddress(address newOracleUpdaterAddress) external;
     function setMultipleValues(string[] memory keys, uint256[] memory compressedValues) external;
 }
 
@@ -13,19 +12,17 @@ interface IDIAOracleV2 {
  * @title DIAOracleV2
  * @dev A simple oracle contract that allows an authorized updater to set and retrieve price values with timestamps.
  */
-contract DIAOracleV2 is IDIAOracleV2 {
+contract DIAOracleV2 is IDIAOracleV2, AccessControl {
+    bytes32 public constant UPDATER_ROLE = keccak256("UPDATER_ROLE");
     /// @notice Mapping to store compressed values of assets (price and timestamp).
     /// @dev The stored value is a 256-bit integer where the upper 128 bits store the price and the lower 128 bits store the timestamp.
     mapping (string => uint256) public values;
-
-    /// @notice Address authorized to update the oracle values.
-    address oracleUpdater;
     
     event OracleUpdate(string key, uint128 value, uint128 timestamp);
     event UpdaterAddressChange(address newUpdater);
     
     constructor() {
-        oracleUpdater = msg.sender;
+        _grantRole(UPDATER_ROLE, msg.sender);
     }
     
      /**
@@ -36,7 +33,7 @@ contract DIAOracleV2 is IDIAOracleV2 {
      * @param timestamp The timestamp associated with the value.
      */
     function setValue(string memory key, uint128 value, uint128 timestamp) public {
-        require(msg.sender == oracleUpdater, "Only the oracleUpdater role can update the oracle.");
+        require(hasRole(UPDATER_ROLE, msg.sender), "Only the oracleUpdater role can update the oracle.");
         uint256 cValue = (((uint256)(value)) << 128) + timestamp;
         values[key] = cValue;
         emit OracleUpdate(key, value, timestamp);
@@ -52,7 +49,7 @@ contract DIAOracleV2 is IDIAOracleV2 {
      */
 
     function setMultipleValues(string[] memory keys, uint256[] memory compressedValues) public {
-        require(msg.sender == oracleUpdater, "Only the oracleUpdater role can update the oracle.");
+        require(hasRole(UPDATER_ROLE, msg.sender), "Only the oracleUpdater role can update the oracle.");
         require(keys.length == compressedValues.length);
         
         for (uint128 i = 0; i < keys.length; i++) {
@@ -77,16 +74,5 @@ contract DIAOracleV2 is IDIAOracleV2 {
         uint128 timestamp = (uint128)(cValue % 2**128);
         uint128 value = (uint128)(cValue >> 128);
         return (value, timestamp);
-    }
-
-    /**
-     * @notice Updates the address of the oracle updater.
-     * @dev Only callable by the current `oracleUpdater`.
-     * @param newOracleUpdaterAddress The new authorized updater address.
-     */
-    function updateOracleUpdaterAddress(address newOracleUpdaterAddress) public {
-        require(msg.sender == oracleUpdater, "Only the oracleUpdater role can update the oracle updater.");
-        oracleUpdater = newOracleUpdaterAddress;
-        emit UpdaterAddressChange(newOracleUpdaterAddress);
     }
 }
