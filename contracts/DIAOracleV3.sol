38753f54d6e770e92d2b1d440b1ff9a2f5b96240
nnn-gif <nicky.gurbani@gmail.com>
2026-03-20 12:21:44 +0530

    fix: update Import and add access control init

--- contracts/DIAOracleV3.sol
+++ contracts/DIAOracleV3.sol
@@ -2,8 +2,8 @@
 pragma solidity 0.8.34;
 
 import {AccessControlUpgradeable} from "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts/proxy/utils/UUPSUpgradeable.sol";
 import "./IDIAOracleV3.sol";
 
 /**
@@ -106,6 +106,7 @@ contract DIAOracleV3 is Initializable, IDIAOracleV3, AccessControlUpgradeable, U
      * @param decimalPrecision The number of decimal places for all asset values.
      */
     function initialize(uint8 decimalPrecision) public reinitializer(1) {
+        __AccessControl_init();
         decimals = decimalPrecision;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(UPDATER_ROLE, msg.sender);
